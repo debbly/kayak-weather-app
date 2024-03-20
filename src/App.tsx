4a5abffc47138169417716199ebe1335b4b6c768
deb <debbs.ly@gmail.com>
2024-03-19 17:00:42 -0700

    fix marquee styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,11 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div className="marquee">
-        <div className="marquee-inner">
+      <div className="wrapper">
+        <div className="marquee">
           <h4>Will I have a good time kayaking?</h4>
           <h4>Will I have a good time kayaking?</h4>
           <h4>Will I have a good time kayaking?</h4>
-          <h4>Will I have a good time kayaking?</h4>
-          <h4>Will I have a good time kayaking?</h4>
-          <h4>Will I have a good time kayaking? Will I have a good time kayaking? Will I have a good time kayaking?</h4>
         </div>
       </div>
       <WeatherInfo />
